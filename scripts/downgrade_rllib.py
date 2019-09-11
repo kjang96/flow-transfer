f7023f1e84e0014043e1f3f4575eac494519d7b4
Kathy Jang <kathyjang@berkeley.edu>
2019-09-11 16:20:04 -0700

    Trying again

--- scripts/downgrade_rllib.py
+++ scripts/downgrade_rllib.py
@@ -18,16 +18,26 @@ if len(sys.argv) != 2:
     print("Exactly 1 arg should be supplied")
     sys.exit()
 
-file = sys.argv[1]
+file_name = sys.argv[1]
 output = sys.argv[1]
 
 # checkpoint_file = 'checkpoint-150' # hardcoded af
 # checkpoint_path = os.path.join(path, output) 
-p = pickle.load(open(file, 'rb'))
-new_assembly = pickle.loads(p['evaluator'])
-p['evaluator'] = pickle.dumps(new_assembly, protocol=2)
+p = pickle.load(open(file_name, 'rb'))
+# import ipdb; ipdb.set_trace()
+# new_assembly = pickle.loads(p['evaluator'])
+new_assembly = pickle.loads(p['worker'])
+# p['evaluator'] = pickle.dumps(new_assembly, protocol=2)
+p['worker'] = pickle.dumps(new_assembly, protocol=2)
 # output = open(new)
-pickle.dump(p, open(file, 'wb'), protocol=2)
+pickle.dump(p, open(file_name, 'wb'), protocol=2)
+
+
+### Now do the same thing with checkpoint.tune_metadata
+file_name = file_name + '.tune_metadata'
+metadata = pickle.load(open(file_name, 'rb'))
+pickle.dump(metadata, open(file_name, 'wb'), protocol=2)
+
 print('Downgrade success')
 
 
